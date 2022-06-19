ca8bff8655f2948393bc398f90ab7b633a28e48e
ismailhamad <ismailhamad2001@gmail.com>
2022-06-19 16:33:10 +0300

    Initial Commit

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Ismail on 19/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
